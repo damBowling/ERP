a6c8198333d8e25ffc0947c732d743df83afa98e
Borja José Celda Gonzalez <geek_person@me.cmo>
2012-09-24 17:08:41 +0200

    Prueba 3

--- Bolera_ERP/src/com/bolera/main/Main.java
+++ Bolera_ERP/src/com/bolera/main/Main.java
@@ -11,18 +11,6 @@ public class Main {
 	 * @param args Irrelevante, de serie
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		//Yo me llamo ralph
-<<<<<<< HEAD
-		//yo si que me llamo ralph
-=======
-=======
-		/**
-		 *hola soy heidy 
-		 * 
-		 */
->>>>>>> prueba
->>>>>>> prueba
 		//Ejemplo de conexión a la base de datos posteriormente probablemente la retiraremos
 		dBm = new DatabaseManager("jdbc:mysql://10.2.34.250:3306/", "florida", "qwerty","heartbreaker");
 	}
